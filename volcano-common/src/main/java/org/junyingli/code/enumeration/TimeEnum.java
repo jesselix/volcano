0cad0925a707d6fd68ed5052b0b619e2c9b9efc7
Junying Li <jesselix@live.com>
2018-03-30 16:58:00 +0800

    =

---
+++ volcano-common/src/main/java/org/junyingli/code/enumeration/TimeEnum.java
@@ -0,0 +1,29 @@
+package org.junyingli.code.enumeration;
+
+public class TimeEnum {
+
+    public enum Week {
+        SUNDAY,
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY
+    }
+
+    public enum Month {
+        JANUARY,
+        FEBRUARY,
+        MARCH,
+        APRIL,
+        MAY,
+        JUNE,
+        JULY,
+        AUGUST,
+        SEPTEMBER,
+        OCTOBER,
+        NOVEMBER,
+        DECEMBER
+    }
+}
