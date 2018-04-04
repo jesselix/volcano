6db2aeb065fd18d8aa1ecb5f10fc376c3af67e28
Junying Li <jesselix@live.com>
2018-04-04 15:17:23 +0800

    =

--- volcano-common/src/main/java/org/junyingli/code/util/RegexUtil.java
+++ volcano-common/src/main/java/org/junyingli/code/util/RegexUtil.java
@@ -1,16 +1,42 @@
 package org.junyingli.code.util;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexUtil {
 
-    public static boolean checkURL(String content) {
-        String regex = "";
-        return Pattern.matches(regex, content);
+    public static boolean isPhoneNumber(String content) {
+        String regex = "^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(content);
+        return matcher.matches();
     }
 
-    public static boolean checkEmail(String content) {
+    public static boolean isEmail(String content) {
+        String regex = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(content);
+        return matcher.matches();
+    }
+
+    public static boolean isSSN(String content) {
+        String regex = "^\\d{3}[- ]?\\d{2}[- ]?\\d{4}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(content);
+        return matcher.matches();
+    }
+
+    public static boolean isNumeric(String content) {
+        String regex = "^[-+]?[0-9]*\\.?[0-9]+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(content);
+        return matcher.matches();
+    }
+
+    public static boolean isChineseId(String content) {
         String regex = "";
-        return Pattern.matches(regex, content);
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(content);
+        return matcher.matches();
     }
 }
--- volcano-common/src/test/java/org/junyingli/code/util/RegexUtilTest.java
+++ volcano-common/src/test/java/org/junyingli/code/util/RegexUtilTest.java
@@ -4,9 +4,34 @@ import org.junit.Test;
 
 public class RegexUtilTest {
 
+    String phone = "1234567890";
+    String email = "_@g.com";
+    String ssn = "123456789";
+    String number = "-4554";
+    String chineseId = "";
+
     @Test
-    public void testCheckURL() {
-        String url = "";
-        System.out.println(RegexUtil.checkURL(url));
+    public void testIsPhoneNumber() {
+        System.out.println(RegexUtil.isPhoneNumber(phone));
+    }
+
+    @Test
+    public void testIsEmail() {
+        System.out.println(RegexUtil.isEmail(email));
+    }
+
+    @Test
+    public void testIsSSN() {
+        System.out.println(RegexUtil.isSSN(ssn));
+    }
+
+    @Test
+    public void testIsNumeric() {
+        System.out.println(RegexUtil.isNumeric(number));
+    }
+
+    @Test
+    public void testIsChineseId() {
+        System.out.println(RegexUtil.isNumeric(chineseId));
     }
 }
