3799dc849b31b405cc82344649abc8a96bf13426
Junying Li <jesselix@live.com>
2018-05-30 17:00:47 +0800

    =

---
+++ volcano-common/src/main/java/org/junyingli/code/datastructure/tree/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,4 @@
+package org.junyingli.code.datastructure.tree.binarysearchtree;
+
+public class BinarySearchTree {
+}
---
+++ volcano-common/src/main/java/org/junyingli/code/datastructure/tree/binarysearchtree/BinarySearchTreeNode.java
@@ -0,0 +1,11 @@
+package org.junyingli.code.datastructure.tree.binarysearchtree;
+
+public class BinarySearchTreeNode<Key extends Comparable<Key>, Value> {
+
+    private Key key;
+    private Value value;
+    private BinarySearchTreeNode leftChild;
+    private BinarySearchTreeNode rightChild;
+
+    
+}
