f7e7d5d57aa5d2f20124148dcfa84ef73db716e8
Junying Li <jesselix@live.com>
2020-04-04 17:20:55 +0800

    +

--- volcano-common/src/main/java/li/jesse/volcanocommon/util/LogUtil.java
+++ volcano-common/src/main/java/li/jesse/volcanocommon/util/LogUtil.java
@@ -1,4 +1,4 @@
-//package li.jesse.volcanocommon.util;
+package li.jesse.volcanocommon.util;//package li.jesse.volcanocommon.util;
 //
 //        import org.slf4j.Logger;
 //        import org.slf4j.LoggerFactory;
