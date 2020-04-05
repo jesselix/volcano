20635ed69989e0c93bc10283f4d9243af5b4d136
Junying Li <jesselix@live.com>
2020-04-05 11:20:44 +0800

    +

---
+++ volcano-common/src/main/java/li/jesse/volcanocommon/util/EncryptionUtil.java
@@ -0,0 +1,24 @@
+package li.jesse.volcanocommon.util;
+import java.security.MessageDigest;
+
+public class EncryptionUtil {
+
+    public static String md5Encryption(String str, String salt) {
+        try {
+            str = str + salt;
+            MessageDigest m = MessageDigest.getInstance("MD5");
+            m.update(str.getBytes("UTF8"));
+            byte s[] = m.digest();
+            String result = "";
+            for (int i = 0; i < s.length; i++) {
+                result += Integer.toHexString((0x000000FF & s[i]) | 0xFFFFFF00).substring(6);
+            }
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "";
+
+    }
+}
